9da8eafa2ded936697bcc5b2627873fcb83acc48
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-10-22 16:38:17 +0900

    v0.3.1
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -17,10 +17,10 @@
 package version
 
 const (
-	Version = "0.3.0+dev"
+	Version = "0.3.1"
 
 	// LatestOfficialStableRelease is used by `norouter show-installer` command.
 	//
 	// The release must be available on https://github.com/norouter/norouter/releases .
-	LatestOfficialStableRelease = "0.3.0"
+	LatestOfficialStableRelease = "0.3.1"
 )
